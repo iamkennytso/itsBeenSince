0e237dcb7dabf006962cdc6d20f90bae6681be3b
iamkennytso <iamkennytso@gmail.com>
2018-07-02 19:54:23 -0400

    (feat) NBA

--- client/App.jsx
+++ client/App.jsx
@@ -18,21 +18,21 @@ class App extends React.Component {
     this.go()
   }
   go(){
-    let grad = new Date (2017, 7, 14)
+    let start = new Date (2018, 7, 2)
     let now = new Date()
-    let months = Math.floor((now - grad) / 2419200000)
-    let weeks = Math.floor((now - grad) % 2419200000 / 604800000)
-    let days = Math.floor((now - grad) % 604800000 / 86400000)
-    let hours = Math.floor(((now - grad) % 86400000) / 3600000)
-    let minutes = Math.floor(((now - grad) % 3600000) / 60000)
-    let seconds = Math.floor(((now - grad) % 60000) / 1000)
+    let months = Math.floor((now - start) / 2419200000)
+    let weeks = Math.floor((now - start) % 2419200000 / 604800000)
+    let days = Math.floor((now - start) % 604800000 / 86400000)
+    let hours = Math.floor(((now - start) % 86400000) / 3600000)
+    let minutes = Math.floor(((now - start) % 3600000) / 60000)
+    let seconds = Math.floor(((now - start) % 60000) / 1000)
     this.setState({months, weeks, days, hours, minutes, seconds})
     setTimeout(this.go, 1000)
   }
   render() {
     return (
       <div id="mainDiv">
-        <img src="recurse.gif" alt='' />
+        <img src="NBA.gif" alt='' />
         <div id="top">It has been </div>
         {this.state.months > 0 ? <div id="months">{this.state.months} {this.state.months === 1 ? 'month' : 'months'} </div> : null}
         {this.state.weeks > 0 ? <div id="weeks">{this.state.weeks} {this.state.weeks === 1 ? 'week' : 'weeks'} </div> : null}
@@ -40,7 +40,7 @@ class App extends React.Component {
         {this.state.hours > 0 ? <div id="hours">{this.state.hours} {this.state.hours === 1 ? 'hour' : 'hours'} </div> : null}
         <div id="minutes">{this.state.minutes} {this.state.minutes === 1 ? 'minute' : 'minutes'} </div>
         <div id="seconds">{this.state.seconds} {this.state.seconds === 1 ? 'second' : 'seconds'} </div>
-        <div id="bottom">SINCE KENNETH HAS HAD A FULL TIME JOB.</div>
+        <div id="bottom">SINCE KENNETH STARTED WORKING AT THE NBA!!!</div>
       </div>
     );
   }
